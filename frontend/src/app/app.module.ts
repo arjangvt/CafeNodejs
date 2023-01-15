4cf65a5ee9adaa684fca2b56e6924375ead29a8a
Aj Fahim <arjangvt@gmail.com>
2023-01-15 11:27:57 -0800

    shered angular module was added

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,19 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './shared/material-module';
 import { HomeComponent } from './home/home.component';
+//import { BestSellerComponent } from './best-seller/best-seller.component';
 
 
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
 export class AppModule { }
